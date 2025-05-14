449a470ce8abca8a30b2e0aa45a7630dbc16c87e
Vinicius Hespanha <mail@vhespanha.dev>
2025-05-14 15:11:31 -0300

    add block data structure
    this data structure constitutes an item in our database, said database
    is our blockchain

---
+++ internal/blockchain/block.go
@@ -0,0 +1,35 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+	}
+	block.SetHash()
+
+	return block
+}
+
+// this hash is not complex enough for blockchain purposes
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
