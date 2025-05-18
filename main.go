751572b7b1943c082a2da4fbebc3cfef2eddf50a
Vinicius Hespanha <mail@vhespanha.dev>
2025-05-18 20:38:52 -0300

    remove unnecessary new line from cli

--- main.go
+++ main.go
@@ -25,9 +25,7 @@ func main() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-
-		pow := blockchain.NewProofOfWork(&block.MetaBlock)
+		pow := blockchain.NewProofOfWork(block)
 		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate(block.Nonce)))
 		fmt.Println()
 	}
